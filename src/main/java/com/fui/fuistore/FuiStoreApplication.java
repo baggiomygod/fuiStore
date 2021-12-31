65d7298d4995d3ae40187e206173a2b452f4998a
wengf <fan1130work@163.com>
2021-12-31 12:07:16 +0800

    'test'

--- src/main/java/com/fui/fuistore/FuiStoreApplication.java
+++ src/main/java/com/fui/fuistore/FuiStoreApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 
 // 加了这段会报错：Consider defining a bean of type 'com.fui.fuistore.repository.BannerRepository' in your configuration.
 //@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-
 @SpringBootApplication
 public class FuiStoreApplication {
 
